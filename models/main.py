caf08d82ba53b9e59d687263598a82a01ebae0db
Samuel Sublate <samuel@asimov.academy>
2024-09-02 21:24:19 +0000

    ajustado o path dos modelos

--- models/main.py
+++ models/main.py
@@ -5,13 +5,13 @@ from inference.stable_diffusion3 import StableDiffusionInference
 load_dotenv()
 
 # FLUX1 INFERENCE
-flux1_inference = Flux1Inference("/home/samuel/models_hf/text-to-image/black-forest-labs-FLUX.1-dev", gpu_index=1, steps=10)
+flux1_inference = Flux1Inference("/mnt/ssd2/models/text-to-image/black-forest-labs-FLUX.1-dev", gpu_index=1, steps=10)
 flux1_image = flux1_inference.run_inference()
 flux1_inference.save_image(flux1_image, "flux1")
 
 del flux1_inference
 
 # STABLE DIFFUSION INFERENCE
-stable_diffusion_inference = StableDiffusionInference("/home/samuel/models_hf/text-to-image/stable-diffusion-3", gpu_index=1)
+stable_diffusion_inference = StableDiffusionInference("/mnt/ssd2/models/text-to-image/stable-diffusion-3", gpu_index=1)
 stable_image = stable_diffusion_inference.run_inference(negative_prompt='')
 stable_diffusion_inference.save_image(stable_image, "stable_diffusion")
